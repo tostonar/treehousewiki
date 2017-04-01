b34e447a1ac121db026a6e5c337f601c17bd66b2
Andrea Toston <tostonar@gmail.com>
2017-04-01 00:55:19 -0400

    Added erb template to show txt files and sinatra route

---
+++ wiki.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
+get '/' do
+  erb :welcome
+end
+
+get "/:title" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :show
+end
